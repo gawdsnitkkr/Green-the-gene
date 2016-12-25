c4a52ca8823298437845a1e9c5090ce798795f92
nkkumawat <nk0kumawat@gmail.com>
2016-12-25 11:52:26 +0530

    partners tab updaed

--- test/js/main.js
+++ test/js/main.js
@@ -1,23 +1,5 @@
-var side_menu_bar = document.getElementById('side-menu-bar');
+var slide = document.getElementById('slide');
 function move_side_bar() {
-	side_menu_bar.style.left="0%";
+	slide.style.left="0%";
+	;console.log("nk")
 }
-// var i=0;
-// var home = document.getElementById('home');
-// var intiatives = document.getElementById('intiatives');
-// var board = document.getElementById('board');                      //team
-// var partners = document.getElementById('partners');
-// var national_directors = document.getElementById('national-directors');
-// var join_us = document.getElementById('join-us');
-// var contributes = document.getElementById('contibutes');
-// var contact = 
-// var tabs = [ home , intiatives , board, partners , national_directors , join_us , contributes ];
-// console.log(tabs.length); 
-
-// function show_tabs(index) {
-// 	for(i=0;i<tabs.length;i++) {
-// 		if(i!=index)
-// 			tabs[i].style.display = "none";
-// 	}
-// 	tabs[index].style.display = "block";
-// }
