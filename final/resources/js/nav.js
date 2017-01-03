73455a3ad010ef03a0739d6c168d4c27d271f9ff
rishabh2609 <rishabhchanana97@gmail.com>
2017-01-03 19:19:27 +0530

    final home page

--- final/resources/js/nav.js
+++ final/resources/js/nav.js
@@ -9,6 +9,11 @@ function showBar() {
 			ease: Power4.easeNone
 		}
 		);
+		TweenMax.to ( "#nav-bar", 0.8, {
+			x: -($('#nav-bar')[0].offsetWidth),
+			ease: Back.easeOut
+		}
+		);
 		TweenMax.to("#path4138-3", 0.5, 
 		{
 			rotationZ: -45,
@@ -17,28 +22,39 @@ function showBar() {
 			ease: Power4.easeNone
 		}
 		);
-		TweenMax.staggerTo("a", 0.2, 
+		TweenMax.staggerTo(".nav-link", 0.1, 
 		{
 			opacity: 1,
-			delay: 0.2
+			delay: 0.5
 		},
-		 0.1
+		 0.05
 		);
 		TweenMax.to ( "#path4138-6", 0.2, 
 		{
 			opacity: 0
 		}
 		);
-		console.dir($('#nav-bar')[0]);
-		$('#nav-bar')[0].style.left = (window.innerWidth - $('#nav-bar')[0].offsetWidth) + 'px';
+		TweenMax.to ( "#home", 0.2, 
+		{
+			opacity: 0.1
+		}	
+		);
+		
 		toggle = 1;
 	}
 	else {
+		TweenMax.staggerTo(".nav-link", 0.1, 
+		{
+			opacity: 0,
+		},
+		 0.05
+		);
 		TweenMax.to("#path4138", 0.5, 
 		{
 			rotationZ: 0,
 			x: 0,
 			y: 0,
+			delay: 0.8,
 			ease: Power4.easeNone
 		}
 		);
@@ -47,18 +63,27 @@ function showBar() {
 			rotationZ: 0,
 			x: 0,
 			y: 0,
+			delay: 0.8,
 			ease: Power4.easeNone
 		}
 		);
-		TweenMax.to ( "#path4138-6", 0.2, 
+		TweenMax.to ( "#path4138-6", 0.5, 
 		{
-			opacity: 1
+			opacity: 1,
+			delay: 1
 		}
 		);
-		$('#nav-bar')[0].style.left = '110%';
-		for( i = 0 ; i < 6 ; i++) {
-			$('a').css('opacity' , '0');
-		}
+		TweenMax.to ( "#home", 0.2, 
+		{
+			opacity: 1,
+			delay: 0.5
+		}	
+		);
+		TweenMax.to ( "#nav-bar", 0.8, {
+			x: ($('#nav-bar')[0].offsetWidth),
+			ease: Back.easeOut,
+			delay: 0.5
+		});
 		toggle = 0;
 	}
 }
