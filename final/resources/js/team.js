e9aeee95f6467f7c147391a7eb261cc96b366f0d
rishabh2609 <rishabhchanana97@gmail.com>
2017-01-08 16:06:38 +0530

    directors added

--- final/resources/js/team.js
+++ final/resources/js/team.js
@@ -74,3 +74,318 @@ $('#close-btn').click(function () {
 		}
 		);
 });
+
+$('#asia').click(function () {
+	TweenMax.to ( "#asia-members", 0.5, 
+		{
+			opacity: 0.8,
+			scale: 1,
+			delay: 0.5,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#europe", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#africa", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#northamerica", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#southamerica", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#top", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#msg-info", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+});
+
+$('#europe').click(function () {
+	TweenMax.to ( "#europe-members", 0.5, 
+		{
+			opacity: 0.8,
+			scale: 1,
+			delay: 0.5,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#asia", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#africa", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#northamerica", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#southamerica", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#top", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#msg-info", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+});
+$('#northamerica').click(function () {
+	TweenMax.to ( "#northamerica-members", 0.5, 
+		{
+			opacity: 0.8,
+			scale: 1,
+			delay: 0.5,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#asia", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#africa", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#europe", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#southamerica", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#top", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#msg-info", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+});
+
+$('#southamerica').click(function () {
+	TweenMax.to ( "#southamerica-members", 0.5, 
+		{
+			opacity: 0.8,
+			scale: 1,
+			delay: 0.5,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#asia", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#africa", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#europe", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#northamerica", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#top", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#msg-info", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+});
+
+$('#africa').click(function () {
+	TweenMax.to ( "#africa-members", 0.5, 
+		{
+			opacity: 0.8,
+			scale: 1,
+			delay: 0.5,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#asia", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#southamerica", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#europe", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#northamerica", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#top", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#msg-info", 0.5, 
+		{
+			opacity: 0,
+			ease: Back.easeOut
+		}
+		);
+});
+
+$('.close-mem-btn').click(function () {
+	TweenMax.to ( "#africa-members", 0.5, 
+		{
+			opacity: 0,
+			scale: 0,
+			ease: Back.easeIn
+		}
+		);
+	TweenMax.to ( "#southamerica-members", 0.5, 
+		{
+			opacity: 0,
+			scale: 0,
+			ease: Back.easeIn
+		}
+		);
+	TweenMax.to ( "#northamerica-members", 0.5, 
+		{
+			opacity: 0,
+			scale: 0,
+			ease: Back.easeIn
+		}
+		);
+	TweenMax.to ( "#asia-members", 0.5, 
+		{
+			opacity: 0,
+			scale: 0,
+			ease: Back.easeIn
+		}
+		);
+	TweenMax.to ( "#europe-members", 0.5, 
+		{
+			opacity: 0,
+			scale: 0,
+			ease: Back.easeIn
+		}
+		);
+
+	TweenMax.to ( "#asia", 0.5, 
+		{
+			opacity: 1,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#southamerica", 0.5, 
+		{
+			opacity: 1,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#europe", 0.5, 
+		{
+			opacity: 1,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#northamerica", 0.5, 
+		{
+			opacity: 1,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#africa", 0.5, 
+		{
+			opacity: 1,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#top", 0.5, 
+		{
+			opacity: 1,
+			ease: Back.easeOut
+		}
+		);
+	TweenMax.to ( "#msg-info", 0.5, 
+		{
+			opacity: 1,
+			ease: Back.easeOut
+		}
+		);
+});
